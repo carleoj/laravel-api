6cf3c0680237e075288916547c1707dc6925c5bd
carleoj <jimroep@gmail.com>
2026-04-25 23:59:48 +0800

    Refactor API routes and implement versioned PostControllers for V1 and V2

---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return 'index from V1';
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return 'store';
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return 'show';
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        return 'update';
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return 'destroy';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\Api\V1\PostController as V1PostController;
+use App\Http\Controllers\Api\V2\PostController as V2PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware(['auth:sanctum']);
 
-Route::get('/hello', function () {
-    return ["message" => "Hello Laravel API"];
+Route::get('/hello', function (Request $request) {
+    return response()->json([
+        "Hello" => "World"
+    ]);
 });
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+Route::prefix('v1')->group(function() {
+    Route::apiResource('posts', V1PostController::class);
+});
+
+Route::prefix('v2')->group(function() {
+    Route::apiResource('posts', V2PostController::class);
+});
+
+
