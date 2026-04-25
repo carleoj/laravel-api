ae1fe210bfd4ae0dce2546a77e11a72aab05f8e5
carleoj <jimroep@gmail.com>
2026-04-25 23:24:47 +0800

    Add PostController and update API routes for posts management

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
+})->middleware(['auth:sanctum']);
+
+Route::get('/hello', function () {
+    return ["message" => "Hello Laravel API"];
 });
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
